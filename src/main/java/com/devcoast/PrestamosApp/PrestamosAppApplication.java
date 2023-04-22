38dd7579408fcc589b38dc8641d4a86297899d5f
marcusdev99 <marcus.sea13@gmail.com>
2023-04-21 22:10:07 -0600

    Creacion de PrestamosApp

---
+++ src/main/java/com/devcoast/PrestamosApp/PrestamosAppApplication.java
@@ -0,0 +1,13 @@
+package com.devcoast.PrestamosApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamosAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devcoast/PrestamosApp/PrestamosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcoast.PrestamosApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
