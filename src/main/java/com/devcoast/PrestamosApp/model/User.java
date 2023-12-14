7da12b08abc2768e1dfc01ca7bec3eccfa82fa3f
marcusdev99 <marcus.sea13@gmail.com>
2023-12-13 18:12:36 -0600

    addition of user entity

---
+++ src/main/java/com/devcoast/PrestamosApp/model/User.java
@@ -0,0 +1,45 @@
+package com.devcoast.PrestamosApp.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String name;
+    private String role;
+
+    public User() {
+    }
+
+    public User(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
